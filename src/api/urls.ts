3ba2eb3839e2da3b577290bb5fe4c17be2d94ab1
Alexs Ruiz <ralexs.acu@gmail.com>
2025-06-22 17:50:44 -0600

    Add URL constants for authentication endpoints

---
+++ src/api/urls.ts
@@ -0,0 +1,8 @@
+export const URLS = {
+  auth: {
+    login: '/auth/signin',
+    register: '/auth/signup',
+    isAuthenticated: '/auth/isAuth',
+    forgotPassword: '/auth/forgotPassword',
+  },
+}
