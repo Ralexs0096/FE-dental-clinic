699d02ad9199e4a5762dbaedd5c87ba230e67233
Alexs Ruiz <ralexs.acu@gmail.com>
2025-06-22 16:47:44 -0600

    Add TypeScript shims for Vue single-file components and path aliases

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+// Add shims for Vue SFCs and path aliases
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const component: DefineComponent<object, object, any>
+  export default component
+}
+
+declare module '@/*'
+declare module '@/pages/*'
+declare module '@/layouts/*'
