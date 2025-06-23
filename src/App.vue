8d832ad3a5cb245a3bd36bb98eff3c2b976c2697
Alexs Ruiz <ralexs.acu@gmail.com>
2025-06-22 18:51:54 -0600

    Implement user authentication check on app mount

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
+import { useAuthStore } from './stores/auth'
+import { isAuth } from './api/auth'
+
+const authStore = useAuthStore()
+
+onMounted(async () => {
+  if (authStore.token && !authStore.user) {
+    const { auth, user } = await isAuth()
+
+    if (auth && user) {
+      authStore.setUser(user)
+      return
+    }
+
+    authStore.logout()
+  }
+})
 </script>
 
 <template>
