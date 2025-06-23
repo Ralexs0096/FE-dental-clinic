c69e8094c0a1ecbe66c14f9704c44150bc73d4c8
Alexs Ruiz <ralexs.acu@gmail.com>
2025-06-22 23:47:43 -0600

    Implement AppointmentsPage component with data fetching and rendering of appointments

--- src/pages/AppointmentsPage.vue
+++ src/pages/AppointmentsPage.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+import { fetchAllAppointments } from '@/api/appointments'
+import type { Appointment } from '@/interfaces/Appointment'
+import { ref } from 'vue'
+import { onMounted } from 'vue'
+
+const appointments = ref<Appointment[]>([])
+
+onMounted(async () => {
+  const appointmentsResponse = await fetchAllAppointments()
+
+  if (appointmentsResponse?.ok) {
+    appointments.value = appointmentsResponse.appointments
+  }
+})
+</script>
+
 <template>
   <h1>Appointments</h1>
+  <div v-for="appointment in appointments" :key="appointment.id">
+    <h2>{{ appointment.title }}</h2>
+  </div>
 </template>
