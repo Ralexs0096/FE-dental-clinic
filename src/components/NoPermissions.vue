e950946ce9b968fbb87bb3fc543961a4dc2ad433
Alexs Ruiz <ralexs.acu@gmail.com>
2025-06-23 22:51:04 -0600

    Add NoPermissions component for handling access restrictions

---
+++ src/components/NoPermissions.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-center px-4">
+    <div class="max-w-md">
+      <h1 class="text-4xl font-bold text-black mb-4">403 - Forbidden</h1>
+      <p class="text-gray-700 text-lg mb-6">You don't have permission to access this page.</p>
+      <RouterLink
+        to="/"
+        replace
+        class="inline-block px-5 py-2 bg-teal-600 text-white rounded hover:bg-teal-700 transition"
+      >
+        Go to Appointments
+      </RouterLink>
+    </div>
+  </div>
+</template>
