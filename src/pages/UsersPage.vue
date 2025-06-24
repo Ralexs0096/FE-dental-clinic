25feb174fe66fa4f2cf86bb055778d99543f91e1
Alexs Ruiz <ralexs.acu@gmail.com>
2025-06-23 22:51:09 -0600

    Add access control to UsersPage, displaying NoPermissions for non-admin users

--- src/pages/UsersPage.vue
+++ src/pages/UsersPage.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { fetchAllUsers, type FetchAllUsers } from '@/api/users'
 import UsersTable from '@/components/UsersTable.vue'
+import { useAuthStore } from '@/stores/auth'
 import { onMounted, ref } from 'vue'
+import NoPermissions from '@/components/NoPermissions.vue'
 
 const users = ref<FetchAllUsers>()
+const authStore = useAuthStore()
 
 onMounted(async () => {
   users.value = await fetchAllUsers()
@@ -11,11 +14,14 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="p-5">
+  <div class="p-5" v-if="authStore.user?.role === 'admin'">
     <h1 class="text-xl font-bold mb-4">Users</h1>
 
     <div class="p-5">
       <UsersTable :users="users?.allUsers ?? []" />
     </div>
+  </div>
+  <div v-else>
+    <NoPermissions />
   </div>
 </template>
