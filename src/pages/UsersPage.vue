4654bb2923ddcdcbeb5fc940ca46086deb0fbd73
Alexs Ruiz <ralexs.acu@gmail.com>
2025-06-23 22:04:28 -0600

    Implement user list display in UsersPage component

--- src/pages/UsersPage.vue
+++ src/pages/UsersPage.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { fetchAllUsers, type FetchAllUsers } from '@/api/users'
+import { onMounted, ref } from 'vue'
+
+const users = ref<FetchAllUsers>()
+
+onMounted(async () => {
+  users.value = await fetchAllUsers()
+})
+</script>
 
 <template>
-  <div>Users</div>
+  <div v-for="user in users?.allUsers" :key="user._id">
+    {{ user.firstName }} {{ user.lastName }}
+  </div>
 </template>
