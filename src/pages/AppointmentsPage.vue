5ed18451c165252d53924ff03f675acaad6234d9
Alexs Ruiz <ralexs.acu@gmail.com>
2025-06-23 21:24:02 -0600

    Refactor AppointmentsPage component to implement table rendering for appointments using @tanstack/vue-table

--- src/pages/AppointmentsPage.vue
+++ src/pages/AppointmentsPage.vue
@@ -1,10 +1,35 @@
 <script setup lang="ts">
-import { fetchAllAppointments } from '@/api/appointments'
+import { ref, onMounted } from 'vue'
 import type { Appointment } from '@/interfaces/Appointment'
-import { ref } from 'vue'
-import { onMounted } from 'vue'
+import { fetchAllAppointments } from '@/api/appointments'
+import { useVueTable, getCoreRowModel } from '@tanstack/vue-table'
 
 const appointments = ref<Appointment[]>([])
+
+const columns = [
+  {
+    accessorKey: 'title',
+    header: 'Title',
+  },
+  {
+    accessorKey: 'description',
+    header: 'Description',
+  },
+  {
+    accessorKey: 'startTime',
+    header: 'Start Time',
+  },
+  {
+    accessorKey: 'endTime',
+    header: 'End Time',
+  },
+]
+
+const table = useVueTable({
+  data: appointments,
+  columns,
+  getCoreRowModel: getCoreRowModel(),
+})
 
 onMounted(async () => {
   const appointmentsResponse = await fetchAllAppointments()
@@ -16,8 +41,35 @@ onMounted(async () => {
 </script>
 
 <template>
-  <h1>Appointments</h1>
-  <div v-for="appointment in appointments" :key="appointment.id">
-    <h2>{{ appointment.title }}</h2>
+  <h1 class="text-xl font-bold mb-4">Appointments</h1>
+
+  <div class="p-5">
+    <table class="table-auto border-collapse border border-gray-400 w-full">
+      <thead>
+        <tr v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
+          <th
+            v-for="header in headerGroup.headers"
+            :key="header.id"
+            class="border border-gray-300 px-4 py-2 bg-gray-100 text-left"
+          >
+            <span v-if="!header.isPlaceholder">
+              {{ header.column.columnDef.header }}
+            </span>
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="row in table.getRowModel().rows" :key="row.id">
+          <td
+            v-for="cell in row.getVisibleCells()"
+            :key="cell.id"
+            class="border border-gray-300 px-4 py-2"
+          >
+            {{ cell.getValue() }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
